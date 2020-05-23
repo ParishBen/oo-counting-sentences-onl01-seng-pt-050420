f7acd1a54ea29ab273e9ce7704278b64486da096
Ben Parish <parishben13@gmail.com>
2020-05-23 16:11:11 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -22,7 +22,7 @@ class String
   if iteratearr.size.nil?
     0 
  # else iteratearr.each.with_index do |words_punctuations, i| if words_punctuations== "?" || words_punctuations== "." || words_punctuations== "!"
-    delete 
+     
    end
   
 
