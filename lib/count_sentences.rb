f61eba8db2fa777cae4f6c9f351cc87697decd04
Ben Parish <parishben13@gmail.com>
2020-05-23 17:11:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,11 +17,11 @@ class String
   end
 
   def count_sentences
-  #iteratearr= self.split(with characters ".?!") 
+                                      #iteratearr= self.split(with characters ".?!") 
   iteratearr = self.split(/\.|\?|\!/)  #this are going to split string & leave with only words & empty spaces
-  if iteratearr.size.nil?
+    if iteratearr.size.nil?
     0 
-  else iteratearr.reject! {|word| word == ""} #taking out empty character values from array
+    else iteratearr.reject! {|word| word == ""} #taking out empty character values from array
   end
-iteratearr.size #returning new array size of only true values of sentences
-end
+ iteratearr.size                               #returning new array size of only true values of sentences
+end 
